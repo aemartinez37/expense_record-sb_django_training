9ca2e8ddae5271eb8f4c1d73c5fa52c6dc83a89b
Andrés Martínez <amartinez@stackbuilders.com>
2022-11-21 17:11:14 -0500

    namespacing URL names

--- expense_record_app/urls.py
+++ expense_record_app/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 from .views import SpenderListView, SpenderDetailView
 
+app_name = 'expense_record'
 urlpatterns = [
-    path('', SpenderListView.as_view(), name='spender-list'),
-    path('<slug:slug>/', SpenderDetailView.as_view(), name='spender-detail'),
+    path('', SpenderListView.as_view(), name='spenders'),
+    path('<slug:slug>/', SpenderDetailView.as_view(), name='detail'),
     path('expenses/<int:spender_id>/',
-         views.expenses_by_spender, name='expenses-by-spender'),
+         views.expenses_by_spender, name='expenses'),
 ]
